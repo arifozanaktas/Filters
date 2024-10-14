396682c369679a863fa154981fba97c88b0740c7
Arif Ozan AKTAŞ <aoaktas@hotmail.com>
2024-10-14 21:42:41 +0300

    Add project files.

---
+++ Filters/Filter/PerformansResourceFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace Filters.Filter;
+
+public class PerformansResourceFilter : Attribute, IResourceFilter
+{
+    private Stopwatch _stopwatch;
+    public void OnResourceExecuted(ResourceExecutedContext context)
+    {
+        _stopwatch = Stopwatch.StartNew();
+    }
+
+    public void OnResourceExecuting(ResourceExecutingContext context)
+    {
+        var elapsedMiliseconds = _stopwatch.ElapsedMilliseconds;
+        var descriptor = context?.ActionDescriptor as ControllerActionDescriptor;
+        if (descriptor != null)
+        {
+            var actionName = descriptor.ActionName;
+            var ctrlName = descriptor.ControllerName;
+
+            Console.WriteLine($"Request çalışma süresi({ctrlName} - {actionName} - {elapsedMiliseconds})");
+
+        }
+    }
+}
